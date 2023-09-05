3b045ceb6aede2e0b0c41174500b6b8ecd8f33c2
himufloyd <114470795+himufloyd@users.noreply.github.com>
2023-09-05 19:39:22 +0530

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,9 @@
+SELECT p.koi_name, p.radius, s.radius
+FROM Star AS s
+JOIN Planet AS p USING (kepler_id)
+WHERE s.kepler_id IN (
+  SELECT kepler_id
+  FROM Star
+  ORDER BY radius DESC
+  LIMIT 5
+);
